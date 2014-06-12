d0aac20c3a14331a466748b5752ecbe2924fd6ab
Preston Mueller <skierjunk@gmail.com>
2014-06-12 12:53:31 -0600

    Fix SeekBar dragging

--- src/net/cactii/flash2/DrawerListAdapter.java
+++ src/net/cactii/flash2/DrawerListAdapter.java
@@ -3,15 +3,20 @@ package net.cactii.flash2;
 import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
 
 public class DrawerListAdapter extends BaseAdapter {
 	
 	private static LayoutInflater inflater = null;
+	private Activity act;
 
 	public DrawerListAdapter(Activity act) {
+		this.act = act;
 		inflater = (LayoutInflater) act.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	}
 	
@@ -40,6 +45,28 @@ public class DrawerListAdapter extends BaseAdapter {
 		if(position == 1)
 		{
 			View view = inflater.inflate(R.layout.drawerstrobe, null);
+			SeekBar seekbar = (SeekBar) view.findViewById(R.id.seekBar1);
+	        seekbar.setOnTouchListener(new ListView.OnTouchListener() 
+	        {
+
+				@Override
+				public boolean onTouch(View v, MotionEvent event) {
+					int action = event.getAction();
+			        switch (action) 
+			        {
+			        case MotionEvent.ACTION_DOWN:
+			            v.getParent().requestDisallowInterceptTouchEvent(true);
+			            break;
+
+			        case MotionEvent.ACTION_UP:
+			            v.getParent().requestDisallowInterceptTouchEvent(false);
+			            break;
+			        }
+
+			        v.onTouchEvent(event);
+			        return true;
+				}
+	        });
 			return view;
 		}
 		return null;
