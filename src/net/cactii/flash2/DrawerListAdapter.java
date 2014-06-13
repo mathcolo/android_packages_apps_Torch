mathcolo/android_packages_apps_Torch

commit 9f5baa606d4512a523f434398b0ca72e9d41dfd0

No high brightness switch if the device doesn't support it.

src/net/cactii/flash2/DrawerListAdapter.java
@@ -27,7 +27,8 @@ public class DrawerListAdapter extends BaseAdapter {
 	
 	@Override
 	public int getCount() {
-		return 3;
+		if(act.getResources().getBoolean(R.bool.hasHighBrightness))return 3;
+		return 2;
 	}
 
 	@Override
@@ -42,6 +43,8 @@ public class DrawerListAdapter extends BaseAdapter {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
+		if(this.getCount() == 2)position++;
+		
 		if(position == 0) {
 			View view = inflater.inflate(R.layout.drawerhighbrightness, null);
 			

commit 69d2355b2607e00e5e935a67f92f23af85653c12

Fixed regression in 9f5baa606d4512a523f434398b0ca72e9d41dfd0 where About item in nav drawer was not functional

src/net/cactii/flash2/DrawerListAdapter.java
@@ -95,6 +95,7 @@ public class DrawerListAdapter extends BaseAdapter {
 		
 		else if(position == 2) {
 			View view = inflater.inflate(R.layout.drawerabout, null);
+			view.setTag("About");
 			return view;
 		}
 		

src/net/cactii/flash2/MainActivity.java
@@ -163,9 +163,9 @@ public class MainActivity extends Activity {
         mDrawerList.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
-				if(arg2 == 2) {
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) {
+				if(view.getTag().equals("About")) {
 					openAboutDialog();
 				}
 				
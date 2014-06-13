69d2355b2607e00e5e935a67f92f23af85653c12
Preston Mueller <skierjunk@gmail.com>
2014-06-13 00:51:41 -0600

    Fixed regression in 9f5baa606d4512a523f434398b0ca72e9d41dfd0 where About item in nav drawer was not functional

--- src/net/cactii/flash2/MainActivity.java
+++ src/net/cactii/flash2/MainActivity.java
@@ -163,9 +163,9 @@ public class MainActivity extends Activity {
         mDrawerList.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
-				if(arg2 == 2) {
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) {
+				if(view.getTag().equals("About")) {
 					openAboutDialog();
 				}
 				
