192b6c11819b490abb160b897140f449ffb04f3a
Mikhail Chowdhury <mchowdhury13@kentdenver.org>
2014-06-13 13:15:42 -0600

    Changed Navigation Drawer Color and Text

--- src/net/cactii/flash2/MainActivity.java
+++ src/net/cactii/flash2/MainActivity.java
@@ -48,8 +48,6 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.SeekBar;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -61,15 +59,6 @@ public class MainActivity extends Activity {
 
     private boolean mBright;
     private boolean mTorchOn;
-
-    // Strobe frequency slider.    
-    private SeekBar mSlider;   
-
-    // Period of strobe, in milliseconds    
-    private int mStrobePeriod;   
-
-    // Label showing strobe frequency    
-    private TextView mStrobeLabel; 
 
     // Preferences
     public SharedPreferences mPrefs;
@@ -144,10 +133,7 @@ public class MainActivity extends Activity {
         mBackgroundShape = (ImageView) findViewById(R.id.bg);
         mLightbulb = (ImageView) findViewById(R.id.lightbulb);
         mLightbulbOn = (ImageView) findViewById(R.id.lightbulb_on);
-        mStrobeLabel = (TextView) findViewById(R.id.strobeTimeLabel);
-        mSlider = (SeekBar) findViewById(R.id.slider);
 
-        mStrobePeriod = 100;
         mTorchOn = false;
 
         mWidgetProvider = TorchWidgetProvider.getInstance();
